43a22b198d96f7ace6e740632486bb5cc480026f
keerthiranji <kiruthiga.ranjithb@gmail.com>
2025-01-07 19:08:29 -0500

    finished problem 3 in Task3

--- src/Task3.java
+++ src/Task3.java
@@ -1,8 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Task3 {
     public static void main(String[] args) {
-
+        //problem 1
         //find maximum value
         int[] array = {2, 6, 11, 20, 3, 7, 18};
         int max = array[0];
@@ -13,7 +15,7 @@ public class Task3 {
         }
         System.out.println(("The maximum value of the array is:  " + max));
 
-
+        //problem 2
         //find minimum value
         int[] array1 = {2, 6, 11, 20, 3, 7, 18};
         int min = array[0];
@@ -23,6 +25,24 @@ public class Task3 {
             }
         }
         System.out.println(("The minimum value of the array is:  " + min));
+
+        //problem 3
+        int[] input = {2,6,11,20,3,7,18};
+        int pivotalValue = 10;
+
+        List<Integer> minArray = new ArrayList<>();
+        List<Integer> maxArray = new ArrayList<>();
+
+        for(int value : input) {
+            if(value <= pivotalValue){
+                minArray.add(value);
+            } else {
+                maxArray.add(value);
+            }
+        }
+
+        System.out.println("Min array:  " + minArray);
+        System.out.println("Max array:  " + maxArray);
 
 
     }
